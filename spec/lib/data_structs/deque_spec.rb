c3b2631b0f84003f7cf99ff50be1ae2382f4522d
Roman Lishtaba <roman@lishtaba.com>
2016-01-03 13:58:44 -0500

    Adding correct rspec annotations

--- spec/lib/data_structs/deque_spec.rb
+++ spec/lib/data_structs/deque_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe Algorithmable::DataStructs::Deque do
+  let(:container) { described_class.new }
+  let(:items) { [1, 2, 3].freeze }
+
+  it 'can instantiate Deque type' do
+    expect(container).to be_kind_of(described_class)
+  end
+
   context '#when testing Deque as LIFO' do
-    let(:container) { described_class.new }
-    let(:items) { [1, 2, 3].freeze }
-
-    it do
-      expect(container).to be_kind_of(described_class)
-    end
-
-    it do
+    it 'can return proper size' do
       container.push_front 1
       container.push_front 2
       expect(container.size).to eq(2)
@@ -25,21 +25,21 @@ describe Algorithmable::DataStructs::Deque do
       expect(container.pop_front).to eq(3)
     end
 
-    it do
+    it 'can handle non existing front peek value' do
       expect(container.peek_front).to be_nil
     end
 
-    it do
+    it 'can raise an exception of expected type' do
       expect { container.pop_front }.to raise_error(described_class::NoSuchElementError)
     end
 
-    it do
+    it 'can pop expected items' do
       items.each { |item| container.push_front item }
       expected_items = container.size.times.collect { container.pop_front }
       expect(expected_items).to eq(items.reverse)
     end
 
-    it do
+    it 'can iterate in reverse order' do
       items.each { |item| container.push_front item }
       local_items = []
       container.reverse_each { |item| local_items << item }
@@ -48,22 +48,11 @@ describe Algorithmable::DataStructs::Deque do
   end
 
   context '#when testing Deque as FIFO' do
-    let(:container) { described_class.new }
-    let(:items) { [1, 2, 3].freeze }
 
-    it do
-      expect(container).to be_kind_of(described_class)
-    end
-
-    it do
+    it 'can push back to deque' do
       container.push_back 1
       container.push_back 2
       expect(container.size).to eq(2)
-    end
-
-    it 'can pop first out' do
-      items.each { |item| container.push_back item }
-      expect(container.peek_front).to eq(1)
     end
 
     it 'can return front peek value' do
@@ -71,26 +60,26 @@ describe Algorithmable::DataStructs::Deque do
       expect(container.peek_front).to eq(1)
     end
 
-    it 'can return front value enqueued lat' do
+    it 'can pop front peek value' do
       items.each { |item| container.push_back item }
-      expect(container.pop_back).to eq(3)
+      expect(container.pop_front).to eq(1)
     end
 
-    it do
+    it 'can handle non existing front peek value' do
       expect(container.peek_front).to be_nil
     end
 
-    it do
+    it 'can raise an exception of expected type' do
       expect { container.pop_front }.to raise_error(described_class::NoSuchElementError)
     end
 
-    it do
+    it 'can yield expected items in forward order' do
       items.each { |item| container.push_back item }
       expected_items = container.size.times.collect { container.pop_front }
       expect(expected_items).to eq(items)
     end
 
-    it do
+    it 'can iterate in forward order' do
       items.each { |item| container.push_back item }
       expect(container.map(&:to_s).join(':')).to eq(items.join(':'))
     end
