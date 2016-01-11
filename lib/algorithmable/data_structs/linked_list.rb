6fe4a4ecb535d2023463b5e83271e260f332163c
Roman Lishtaba <roman@lishtaba.com>
2016-01-11 18:11:28 -0500

    Adding basic implementaiton of LL

--- lib/algorithmable/data_structs/linked_list.rb
+++ lib/algorithmable/data_structs/linked_list.rb
@@ -1,98 +1,144 @@
 module Algorithmable
   module DataStructs
     class LinkedList
-      include Enumerable
-      attr_reader :length
-
-      def initialize
-        @head = nil
-        @tail = nil
-        @length = 0
-        @op_counter = 0
+      attr_reader :size
+      def initialize(collection = [])
+        @front = nil
+        @back = nil
+        @size = 0
+        collection.each { |item| push_front item }
       end
 
-      def unshift(data)
-        link_head_node data
+      def empty?
+        0 == size
       end
 
-      def push(data)
-        link_tail_node data
+      def clear
+        @front = nil
+        @back = nil
+        @size = 0
       end
 
-      def first
-        @head ? @head.dup : fail_index_error
+      def peek_front
+        @front && @front.item
       end
 
-      def last
-        @tail ? @tail.dup : fail_index_error
+      def peek_back
+        @back && @back.item
       end
 
-      def each(&block)
-        next_node = @head
-        nodes = []
-        while next_node
-          nodes << next_node
-          next_node = next_node.succ
+      def key?(obj)
+        each.map(&:item).include? obj
+      end
+
+      def pop_front
+        return unless @front
+        node = @front
+        if 1 == @size
+          clear
+          return node.item
+        else
+          @front = @front.next
         end
-        nodes.each(&block)
+        @size -= 1
+        node.item
+      end
+
+      def push_front(obj)
+        node = Node.new(obj)
+        if @front
+          node.next = @front
+          @front = node
+        else
+          @front = node
+          @back = @front
+        end
+        @size += 1
+        obj
+      end
+
+      def push_back(obj)
+        node = Node.new(obj)
+        if @back
+          @back.next = node
+          @back = node
+        else
+          @front = node
+          @back = @front
+        end
+        @size += 1
+        obj
+      end
+
+      def pop_back
+        return unless @back
+        node = @back
+
+        if 1 == @size
+          clear
+          return node.item
+        else
+          current = @front
+          current = current.next until current.next.next.nil?
+          current.next = nil
+          @back = current
+        end
+
+        @size -= 1
+        node.item
+      end
+
+      def delete(item)
+        tmp_node = @front
+        prev_node = nil
+
+        until tmp_node.nil?
+          if tmp_node.item == item
+            if tmp_node == @front
+              unlink_head_node
+            else
+              unlink_node prev_node, tmp_node
+            end
+            return true
+          else
+            prev_node = tmp_node
+          end
+
+          tmp_node = tmp_node.next
+        end
       end
 
       private
 
-      def link_head_node(data)
-        prev_head = @head
-        node = make_node data, nil, prev_head
-        @head = node
+      def unlink_head_node
+        next_node = @front.next
+        @back = next_node if @front == @back
+        @front = next_node
+        @size -= 1
+      end
 
-        if prev_head
-          prev_head.pred = node
-        else
-          @tail = node
+      def unlink_node(prev_node, node)
+        prev_node.next = node.next
+        @back = prev_node if node == @back
+        @size -= 1
+      end
+
+      def each(&block)
+        nodes = []
+        node = @front
+        until node.nil?
+          nodes << node
+          node = node.next
         end
-
-        bump_length!
-        bump_op_counter!
-      end
-
-      def link_tail_node(data)
-        prev_tail = @tail
-        node = make_node data, prev_tail, nil
-        @tail = node
-
-        if prev_tail
-          prev_tail.succ = node
-        else
-          @head = node
-        end
-
-        bump_length!
-        bump_op_counter!
-      end
-
-      def make_node(data, pred = nil, succ = nil)
-        Node.new data, pred, succ
-      end
-
-      def bump_length!
-        @length = @length.next
-      end
-
-      def bump_op_counter!
-        @op_counter = @op_counter.next
-      end
-
-      def fail_index_error(message = nil)
-        fail IndexError, message
+        nodes.each(&block)
       end
 
       class Node
-        attr_accessor :pred, :succ
-        attr_reader :data
+        attr_accessor :item, :next
 
-        def initialize(data, pred = nil, succ = nil)
-          @data = data
-          @pred = pred
-          @succ = succ
+        def initialize(item, pointer = nil)
+          @item = item
+          @next = pointer
         end
       end
       private_constant :Node
