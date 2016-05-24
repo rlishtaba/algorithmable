cc8ebb594000f37d23b611868b846dddbc69e35e
Roman Lishtaba <roman@lishtaba.com>
2016-05-24 17:11:58 -0400

    Allocating namespace for tree ds

--- lib/algorithmable/data_structs/ordered_symbol_table.rb
+++ lib/algorithmable/data_structs/ordered_symbol_table.rb
@@ -2,12 +2,14 @@ module Algorithmable
   module DataStructs
     class OrderedSymbolTable
       extend Forwardable
+      include Algorithmable::Trees
+
+      private_methods :new_binary_search_tree
 
       def_delegators :@imp, :[]=, :[], :key?, :empty?, :size, :keys, :max, :min, :floor, :ceiling, :rank, :delete
 
       def initialize(key_type, value_type)
-        search_strategy_factory = Object.new.extend Algorithmable::Searches
-        @imp = search_strategy_factory.new_binary_search_tree(key_type, value_type)
+        @imp = new_binary_search_tree(key_type, value_type)
       end
     end
   end
